bfdf126ad5a0e46ccdeb3f7ee1b4c42be0864e51
ana4is <anaisflechier@gmail.com>
2022-08-26 08:57:37 +0200

    Installation React

---
+++ dictionary-react-project/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <button type="button" class="btn btn-info">
+        Info
+      </button>
+    </div>
+  );
+}
+
+export default App;
